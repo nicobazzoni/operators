15b576e7fee53b87fb995afd81bc265d781eff93
Nico Bazzoni <you@example.com>
2020-11-11 22:22:37 -0500

    commit tree afc98c05d06268ef5946d75c34865580b61365e1

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-
+    speed < 40 || speed > 60 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+  if  speed > 40 || speed < 60 ? true : false
+  elsif speed.between?(40, 60) 
+    return false
 end
 	
 
